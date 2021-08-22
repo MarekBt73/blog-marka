acff12cb8fc2257279b9f9d8eecd0c50112c0d01
Marek Becht <marek.becht@gmail.com>
2021-08-23 00:06:33 +0200

    jaki-223

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,34 +1,64 @@
 import React from 'react'
-import styled from 'styled-components';
+import styled, { createGlobalStyle } from 'styled-components';
+
+const GlobalStyle = createGlobalStyle`
+  body {
+    padding: 0;
+    margin: 0;
+    font-family: 'Montserrat';
+    color: white;
+  }
+  
+  *, *::before, *::after {
+    box-sizing: border-box;
+  }
+`;
 
 const StyledWrapper = styled.div`
 
-@media all and (max-width: 760px) and (min-width: 320px){
-max-width:480;
-height: 100%;
-background-color: #999999;
+
+margin: 0 auto;
 display: flex;
 flex-direction: column;
 justify-content: center;
 align-items:center;
 padding: 25px;
+min-height: 100vh;
+
+@media all and (max-width: 760px) and (min-width: 10px){
+max-width:480;
+background-color: #999999;
+
+}
+@media all and (max-width: 1024px) and (min-width: 761px){
+max-width:760px;
+background-color: #777777;
+
+}
+@media (min-width: 1025px){
+max-width:1000px;
+background-color: #999999;
 }
 `
+const H1 = styled.h1`
+color: black;
+font-size: 52px;
+`
+const Pstyle = styled.p`
+color: white;
+font-size: 32px;
+`
 
-
-const IndexPage = () => {
-return(
-  <StyledWrapper>
-
-<h1>
-  Blog Marka
-</h1>
-<p>
-  Witam na blogu o układaniu płytek, malowaniu i remontach mieszkań.
-</p>
-
+const IndexPage = () => (
+  <>
+  <GlobalStyle/>
+  <StyledWrapper className="roman">
+    <H1>Blog- Marka</H1>
+    <Pstyle>Opowiadam w wykończeniach wnetrz</Pstyle>
   </StyledWrapper>
+  </>
 )
-}
 
 export default IndexPage
+
+
